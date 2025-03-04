07c764b62a1f59e4ddf789d2bf8d58af5e613458
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-04 12:45:55 +0530

    Creating Main.java
    Defining the initial code block of class Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Name: Ameya Tipnis
+//PRN: 23070126012
+//Batch: 2023-27 (A1)
+
+// Main.java
+import java.util.*; // Importing Scanner and ArrayList utilities
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); // Creating a scanner object for user input
+        StudentOperations operations = new StudentOperations(); // Creating an instance of StudentOperations to manage student records
+        int choice;
