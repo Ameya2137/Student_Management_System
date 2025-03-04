3fbf8f4a6a1a51d2632744e3a109bf3a4d876822
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-04 16:05:18 +0530

    Update Student.java
    Defining a constructor to initialize Student object

--- Student.java
+++ Student.java
@@ -6,4 +6,14 @@ class Student { // Defining a class named Student
     private String branch;
     private String batch;
     private float cgpa;
+
+    // Defining a constructor that initializes a Student object with the given values
+    public Student(String name, int prn, String branch, String batch, float cgpa) {
+        // this.variablename is used to assign parameter values to instance variables
+        this.name = name;
+        this.prn = prn;
+        this.branch = branch;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
 }
