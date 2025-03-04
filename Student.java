0d09e508085981bc721dca317cbc431ff1c16b32
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-04 16:00:05 +0530

    Create Student.java
    Creating a java file called Student that defines the Student class and its attributes.

---
+++ Student.java
@@ -0,0 +1,9 @@
+// Student.java
+class Student { // Defining a class named Student
+    // Defining the attributes or instance variables of Student
+    private String name;
+    private int prn;
+    private String branch;
+    private String batch;
+    private float cgpa;
+}
