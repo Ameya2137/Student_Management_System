836887b68980fab95a732677a6e2d26a52f1d0ea
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-04 18:23:22 +0530

    Update StudentOperations.java
    Added functions:
    - Add Students
    - Display Students

--- StudentOperations.java
+++ StudentOperations.java
@@ -5,3 +5,10 @@ class StudentOperations { // Defining a StudentOperations class that manages a l
     private ArrayList<Student> students; // Declaring a private list that stores Student objects directly
 
     public StudentOperations() { this.students = new ArrayList<>(); } // Constructor that initializes an empty student list
+
+    public void addStudent(Student student) { students.add(student); } // Adds an object to the list
+
+    public void displayStudents() {
+        for (Student student : students) { student.display(); } // Iterates through all objects and prints the student details
+    }
+}
